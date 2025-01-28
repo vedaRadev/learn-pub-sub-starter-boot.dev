2f5d7d1b7fc510d0abeb39e36b9443b584c52222
vedaRadev <ryancarmstrong97@gmail.com>
2025-01-28 10:52:09 -0500

    simple server repl

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
     "fmt"
-    "os"
-    "os/signal"
     "github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
     "github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+    "github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
     amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	fmt.Println("Starting Peril server...")
+    gamelogic.PrintServerHelp()
+
     connectionString := "amqp://guest:guest@localhost:5672/"
     connection, err := amqp.Dial(connectionString)
     if err != nil {
@@ -26,15 +26,42 @@ func main() {
         return
     }
 
-    err = pubsub.PublishJSON(
-        connectionChannel,
-        routing.ExchangePerilDirect,
-        routing.PauseKey,
-        routing.PlayingState { IsPaused: true },
-    )
+    for {
+        input := gamelogic.GetInput()
+        if len(input) == 0 { continue }
 
-    osSignals := make(chan os.Signal, 1)
-    signal.Notify(osSignals, os.Interrupt)
-    <-osSignals
-    fmt.Println("Shutting down and closing rabbitmq server connection")
+        switch input[0] {
+
+        case "pause":
+            fmt.Println("Sending pause message")
+            err = pubsub.PublishJSON(
+                connectionChannel,
+                routing.ExchangePerilDirect,
+                routing.PauseKey,
+                routing.PlayingState { IsPaused: true },
+            )
+            if err != nil {
+                fmt.Println("Failed to publish pause message to exchange")
+            }
+
+        case "resume":
+            fmt.Println("Sending resume message")
+            err = pubsub.PublishJSON(
+                connectionChannel,
+                routing.ExchangePerilDirect,
+                routing.PauseKey,
+                routing.PlayingState { IsPaused: false },
+            )
+            if err != nil {
+                fmt.Println("Failed to publish resume message to exchange")
+            }
+
+        case "quit":
+            fmt.Println("Exiting")
+            break
+
+        default:
+            fmt.Printf("Unrecognized command: %v\n", input[0])
+        }
+    }
 }
